d61d4a57cf08cb48bdbe2110e819b337e7276e93
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-29 10:06:47 +0330

    add table

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ import snscrape.modules.twitter as sntwitter
 app = Flask(__name__)
 
 
-query = "(from:vitalik.eth) since:2022-09-01"
+query = "(from:VitalikButerin) since:2022-09-01 -filter:replies"
 tweets = []
+
+query_2 = "vitalik buterin 'vitalik buterin' since:2022-09-01 -filter:links -filter:replies"
+tweets_2 = []
 
 sesssion = HTMLSession()
 r = sesssion.get("https://vitalik.ca/")
 
-r.html.render(timeout=10000 ,scrolldown=5)
+r.html.render(timeout=20,scrolldown=5)
 
 viloger= r.html.find('h3')
 @app.route("/" , methods = ["POST", "GET"])
@@ -24,12 +27,18 @@ def index():
             link = article.absolute_links
         
             for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-                if len(tweets) == 10:
+                if len(tweets) == 5:
                     break
                 else:    
                     tweets.append([tweet.date, tweet.user.username, tweet.content])
+            
+            for tweet in sntwitter.TwitterSearchScraper(query_2).get_items():
+                if len(tweets_2) == 3:
+                    break
+                else:    
+                    tweets_2.append([tweet.date, tweet.user.username, tweet.content])
                          
-            return render_template("index.html",title=title, link=link, item=item, viloger=viloger, tweets=tweets)    
+            return render_template("index.html",title=title, link=link, item=item, viloger=viloger, tweets=tweets,tweets_2=tweets_2)    
         except:
             pass
 
