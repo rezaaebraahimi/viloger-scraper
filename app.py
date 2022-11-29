39b83a189e21dd6c0850d9de8524fafb327a77d4
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-29 07:27:58 +0330

    add twitter scraper

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 from flask import Flask, render_template
+from requests_html import HTMLSession
+import snscrape.modules.twitter as sntwitter
+
+
+
 
 app = Flask(__name__)
 
-from requests_html import HTMLSession
+
+query = "(from:vitalik.eth) since:2022-09-01"
+tweets = []
+limit = 10
+
+
 sesssion = HTMLSession()
 r = sesssion.get("https://vitalik.ca/")
 
 r.html.render(timeout=10000 ,scrolldown=5)
 
-viqoulik= r.html.find('h3')
-articlelist = []
+viloger= r.html.find('h3')
 @app.route("/" , methods = ["POST", "GET"])
 def index():
-    for item in viqoulik:
+    for item in viloger:
         try:
             article = item.find('a',first=True)
             title = article.text 
             link = article.absolute_links
-            articles = {
-                "title" : title,
-                "link" : link
-            }
-            articlelist.append(articles)
-            return render_template("index.html", title=title, link=link, item=item, viqoulik=viqoulik )
+        
+            for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+                if len(tweets) == limit:
+                    break
+                else:    
+                    tweets.append([tweet.date, tweet.user.username, tweet.content])
+                         
+            return render_template("index.html",title=title, link=link, item=item, viloger=viloger, tweets=tweets)    
         except:
             pass
-app.run(host="0.0.0.0")
+
+
+if __name__ == '__main__':
+    app.run()
